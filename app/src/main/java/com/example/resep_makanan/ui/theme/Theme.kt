c691e3c16761bc9581784ed0cd9440d842ed5b1e
AlIhsanAli <2300018178@webmail.uad.ac.id>
2025-11-09 14:45:32 +0700

    hehe

--- app/src/main/java/com/example/resep_makanan/ui/theme/Theme.kt
+++ app/src/main/java/com/example/resep_makanan/ui/theme/Theme.kt
@@ -1,11 +1,8 @@
 package com.example.resep_makanan.ui.theme
 
-import android.app.Activity
 import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
@@ -39,11 +36,13 @@ fun BearLoginTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
+    // Force light theme, but still allow dynamic light colors on Android 12+
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
             dynamicLightColorScheme(context)
         }
+
         else -> LightColorScheme
     }
 
